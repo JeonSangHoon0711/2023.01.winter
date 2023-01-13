822811a383c2609752811f1270e92d1e335f6137
JeonSangHoon0711 <121441565+JeonSangHoon0711@users.noreply.github.com>
2023-01-13 13:48:55 +0900

    Add files via upload

---
+++ CafeSeating.java
@@ -0,0 +1,7 @@
+
+public class CafeSeating {
+	
+	public void go() {
+		
+	}
+}
---
+++ CafeSeatingDriver.java
@@ -0,0 +1,8 @@
+
+public class CafeSeatingDriver {
+	public static void main(String[] args) {
+		CafeSeating cafeseating = new CafeSeating();
+		cafeseating.go();
+	}
+
+}
