297568182da81b7d20aaf18e0d70e7f3edd44698
minyoung kim <nonamed1021@naver.com>
2023-01-16 13:29:09 +0900

    upload

---
+++ backgroundPanel.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+class backgroundPanel extends JPanel{
+	private Image img;
+	
+	public backgroundPanel(Image img) {
+		this.img = img;
+		setSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+		setLayout(null);
+	}
+	
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+	}
+}
