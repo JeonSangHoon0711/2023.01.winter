056a3ddabf6e21bc7bfd4b5fb13b1227e5971a61
minyoung kim <nonamed1021@naver.com>
2023-01-16 16:41:43 +0900

    update 5

--- notification.java
+++ notification.java
@@ -4,18 +4,20 @@ import java.awt.Font;
 import javax.swing.JLabel;
 
 class notification extends JLabel implements Runnable{
-	Thread Th; boolean On = false;
+	Thread Th; boolean On;
 	int X, Y, Width, Height;
 	
 	public notification(String str) {
 		Th = new Thread(this, "notification");
 		X = 0; Y = 0; Width = 10; Height = 10;
+		On = false;
 		setText(str);
 		setHorizontalAlignment(JLabel.CENTER);
 		setBackground(Color.WHITE);
-		setFont(new Font("±¼¸²",Font.BOLD,18));
+		setFont(new Font("맑은 고딕",Font.BOLD,18));
 		setOpaque(true);
 		setBounds(X, Y, Width, Height);
+		setVisible(false);
 		Th.start();
 	}
 	
@@ -24,12 +26,17 @@ class notification extends JLabel implements Runnable{
 			if(On) {
 				setVisible(true);
 				try {
-					Thread.sleep(5000);
+					Thread.sleep(3000);
 				} catch (InterruptedException e) {
 					;
 				}
 				setVisible(false);
 				On = false;
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				;
 			}
 		}
 	}
