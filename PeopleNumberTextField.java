84178688f07f4e8b06c25318d0df205682f87820
JeonSangHoon0711 <wjstkdgns711@gmail.com>
2023-01-18 14:00:36 +0900

    colorchange

--- PeopleNumberTextField.java
+++ PeopleNumberTextField.java
@@ -10,11 +10,12 @@ public class PeopleNumberTextField {
 	public PeopleNumberTextField() {
 		
 		peoplenumbertextfield.setHorizontalAlignment(JLabel.CENTER);
-		peoplenumbertextfield.setBackground(Color.pink);
+		
 		peoplenumbertextfield.setFont(new Font("맑은 고딕", Font.BOLD, 15));
 		peoplenumbertextfield.setBounds(390,20,90,45);
 		peoplenumbertextfield.setBorder(null);
-		
+		peoplenumbertextfield.setBackground(new Color(124, 190, 249));
+		peoplenumbertextfield.setForeground(Color.WHITE);
 	}
 	public JTextField getPeoplenumbertextfield() {
 		return peoplenumbertextfield;
